435f475c4ef9937ad70a2ec636f52a6c464b85ec
Nikita Elakov <89816960858@mail.ru>
2020-09-19 00:49:44 +0300

    page object and test cases

--- src/test/java/page/LoginPage.java
+++ src/test/java/page/LoginPage.java
@@ -2,10 +2,8 @@ package page;
 
 import com.codeborne.selenide.SelenideElement;
 import data.DataHelper;
-import org.junit.jupiter.api.Test;
-
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+
 
 public class LoginPage<val> {
     private SelenideElement loginField = $("[data-test-id=login] input");
